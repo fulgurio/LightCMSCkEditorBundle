4998a4a7dbee58daef4eecdbe48569ea2c7e0ac7
fulgurio <v.guerard@fulgurio.net>
2014-07-05 00:33:46 +0200

    Adding CKeditor File

---
+++ DependencyInjection/FulgurioLightCMSCkEditorExtension.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of the LightCMSCkEditorBundle package.
+ *
+ * (c) Fulgurio <http://fulgurio.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fulgurio\LightCMSCkEditorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class FulgurioLightCMSCkEditorExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $container->setParameter('fulgurio_light_cms_ck_editor', $config);
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ FulgurioLightCMSCkEditorBundle.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the LightCMSCkEditorBundle package.
+ *
+ * (c) Fulgurio <http://fulgurio.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fulgurio\LightCMSCkEditorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FulgurioLightCMSCkEditorBundle extends Bundle
+{
+}
